52168b998636f17dd522fff80a0834d3da3d58ba
levuloihust99 <levuloihust@outlook.com>
2022-10-31 11:28:44 +0700

    parse params when flatten vlsp2022 train data

--- seq2seq/flatten_multi_to_single_summ.py
+++ seq2seq/flatten_multi_to_single_summ.py
@@ -1,9 +1,5 @@
-import csv
+import argparse
 import json
-from datasets import Dataset
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainer, TrainingArguments, Seq2SeqTrainingArguments
-from tqdm import tqdm
-from torch.utils.data import DataLoader
 
 
 def preprocess_fn(tokenizer, features):
@@ -21,12 +17,15 @@ def preprocess_fn(tokenizer, features):
 
 
 def main():
-    model_name = "VietAI/vit5-base-vietnews-summarization" # or "VietAI/vit5-large-vietnews-summarization"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-path", required=True,
+                        help="Path to the multi-document summarization dataset, in jsonlines format.")
+    parser.add_argument("--output-path", required=True,
+                        help="Path to the output flatten document-summary pair file, in jsonlines format.")
+    args = parser.parse_args()
 
-    data_path = "/media/lvloi/projects/vlsp-2022/data/vlsp-2022/jsonl/vlsp_2022_abmusu_train_data_new.jsonl"
     data = []
-    with open(data_path, "r") as reader:
+    with open(args.input_path, "r") as reader:
         for line in reader:
             data.append(json.loads(line.strip()))
 
@@ -38,8 +37,7 @@ def main():
         documents.extend(raw_texts)
         summaries.extend([item['summary'] for _ in range(len(raw_texts))])
     
-    output_path = "/media/lvloi/projects/vlsp-2022/data/vlsp-2022/jsonl/vlsp_2022_abmusu_train_data_flattened.jsonl"
-    writer = open(output_path, 'w')
+    writer = open(args.output_path, 'w')
     for doc, summary in zip(documents, summaries):
         writer.write(json.dumps({'document': doc, 'summary': summary}, ensure_ascii=False) + "\n")
     writer.close()
